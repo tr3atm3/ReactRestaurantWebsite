b1bdd27d40ac6f132d44ceeada9c076662a96e10
tr3atm3 <tttushar.789@gmail.com>
2024-02-22 21:51:46 +0530

    mealItemAdded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
-import MealsSummary from "./components/Meals/MealsSummary";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <MealsSummary />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 const Meals = () => {
-  return <div>Meals</div>;
+  return (
+    <React.Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </React.Fragment>
+  );
 };
 
 export default Meals;
