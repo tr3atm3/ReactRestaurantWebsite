6d41fdf3f58b3faefc3bd4702f16be9d9fdda6fc
tr3atm3 <tttushar.789@gmail.com>
2024-02-23 16:40:09 +0530

    contextToAddItem

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const nofCartItems = cartCtx.items.reduce((acc, cur) => {
+    return acc + cur.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span>Your cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>{nofCartItems}</span>
     </button>
   );
 };
